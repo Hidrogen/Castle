47365d27a461e06e6b85c11884a8e1d5d130a18f
Hidrogen <raimujica@outlook.com>
2013-12-16 17:21:15 -0300

    CA 0.2
    Map loader updates. Works badly.

--- Map Loader/tile_reader.py
+++ Map Loader/tile_reader.py
@@ -13,24 +13,34 @@ def load_tiles(filename, w, h):
         for i in range(int(tilesheet_height/h)):
             tile_table[j].append(tilesheet.subsurface((i*w,h*j,w,h)))
     return tile_table
-def load_csv(filename):
+
+def load_csv(filename, tile_table):
     layer = open(filename)
+    column = 0
+    row = 0
     for line in layer:
         line = line.split(",")
         for element in line:
-            #if "0":
-                #blit la tile correspondiente a 0. (vacio en este caso)
+            if element == "0":
+                screen.blit(tile_table[0][0], (row*32, column*32))
+            if element == "167":
+                screen.blit(tile_table[10][7], (row*32, column*32))
+            if element == "183":
+                screen.blit(tile_table[11][7], (row*32, column*32))
+            row += 1
+            if row >= 16:
+                row = 0
+        column += 1
 
 if __name__=='__main__':
     pygame.init()
     screen = pygame.display.set_mode((800, 600))
     screen.fill((250, 250, 250))
-    load_csv("mapCSV_Group1_Ground.csv")
     table = load_tiles("Castle Tiles.png", 32, 32)
-    for y, row in enumerate(table):
-        for x, tile in enumerate(row):
-            screen.blit(tile, (x*32, y*32))
+##    for y, row in enumerate(table):
+##        for x, tile in enumerate(row):
+##            screen.blit(tile, (x*32, y*32))
+    load_csv("mapCSV_Group1_Ground.csv", table)
     pygame.display.flip()
     while pygame.event.wait().type != pygame.locals.QUIT:
         pass
-#este codigo lo saque de wiki sheep. No es mio.
